aecb681e79f332323871faf381b967a6e622f475
Mahmoud Ramadan <95087747+mahmoudramadan0040@users.noreply.github.com>
2022-06-04 18:44:46 +0200

    Update ApiProductController.php

--- server/app/Http/Controllers/ApiProductController.php
+++ server/app/Http/Controllers/ApiProductController.php
@@ -97,7 +97,7 @@ class ApiProductController extends Controller
                                 'qty' => $item->qty,
                                 'sku' => $item->sku,
                                 'stock_status' => $item->stock_status,
-                                'image1' => env('AWS_URL') . '/' . $item->image1,
+                                'image1' => str_replace('//', "\\","http://ecommerce-2.s3-website-us-east-1.amazonaws.com") .'\\'. $item->image1,
                                 'image2' => env('AWS_URL') . '/' . $item->image2,
                                 'image3' => env('AWS_URL') . '/' . $item->image3,
                                 'image4' => env('AWS_URL') . '/' . $item->image4,
